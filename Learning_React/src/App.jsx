167a40b0eabfb33c6867f19602714dc19173a4a1
Saphaa mohamed <saphaasaphaamohamed@gmail.com>
2025-03-03 13:52:31 +1400

    UseRef

--- Learning_React/src/App.jsx
+++ Learning_React/src/App.jsx
@@ -2,65 +2,26 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef } from 'react'
 
 function App() {
-
-
-  //  const counter = useState(0) [0]
-  //  const setCounter = useState(0) [1]
-  //  console.log(counter);
-  //  console.log(setCounter);
-  // const [color, setcolor] = useState('red')
-  // const changeColor = () => {
-  //    color = 'blue'
-  //    console.log(color);
-  //    setcolor('blue');
-  // }
-  
-  // const[brand, setBrand] = useState('Ferrari');
-  // const[model, setModel] = useState('Roma');
-  // const[year, setYear] = useState(2025);
-  // const[color, setColor] = useState('Red');
-  // const [car, setCar] = useState({
-  //   brand: 'Ferrari',
-  //   model: 'Roma',
-  //   year: 2025,
-  //   color: 'Red'
-  // });
-
-  // const changeColor = () => {
-  //   setCar((prev)=>{
-  //     return {...prev, color: 'Blue'}
-  //   })
-  // }
-  // const [count, setCount] = useState(0);
-  // const increaseCount = () => {
-    // setCount(count + 1);
-
-    // setCount(count=> count+ 1);
-    // setCount(count=> count + 1);
-    // setCount(count=> count + 1);
-    // setCount(count=> count+ 1);
-  // }
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('saphaa');
-  useEffect(() => {
-    setTimeout(() => {
-      setCount( count => count + 1);
-    }, 2000);
-  } ,[count, name]);
+const [value, setValue] = useState(0)
+// const [count, setCount] = useState(0)
+// useEffect(()=>{
+//   setCount(prev => prev +1)
+// })
+const count = useRef(10);
+// console.log(count) ;
+useEffect(()=>{
+  count.current= count.current +1;
+});
   return (
     <>
 
-   {/* <h1>my favourate color is {color}!</h1>
-       <button>Blue</button> */}
-       {/* <h1>My { car.brand}</h1>
-       <h2>It is a{car.color} { car.model} from {car.year}</h2>
-       <button onClick={changeColor}>Blue</button> */}
-       {/* <h1>Count: {count}</h1>
-       <button onClick={increaseCount}> Increase</button> */}
-        <h1>I've rendered {count} times!</h1>
-       
+  <button onClick={()=>{setValue (prev => prev -1)}}>-1</button>
+  <h1>{value}</h1>
+  <button onClick={()=>{setValue (prev => prev +1)}}>+1</button>
+       <h1>Render Count: {count.current}</h1>
     </>
   )
 }
